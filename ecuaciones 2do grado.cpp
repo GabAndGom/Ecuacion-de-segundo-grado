6a804f250ab088d333b1a64f91231241817e56c7
Gabriel Gomez <andresgabrielgc@gmail.com>
2022-02-22 21:57:06 -0400

    arreglos finales

--- ecuaciones 2do grado.cpp
+++ ecuaciones 2do grado.cpp
@@ -2,6 +2,7 @@
 // calcularemos mediante la formula general para ecuaciones de segundo grado de la forma Ax^2+Bx+C=0
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
 using namespace std;
 int main()
 {
@@ -15,40 +16,42 @@ int main()
     cout<<"C: ";
     cin>>C;
     aux1=(pow(B,2)-(4*A*C));
-    if (aux1<0)
+    if(A==0)
     {
+        cout<<"la ecuacion no es de segundo grado ,la solucion es: "<<C/B;
+    }
+    else
+    {
+        if (aux1<0)
+        {
         cout<<"la ecuacion no tiene solucion en los reales "<<endl;
         xreal=-B/(2*A);
         ximag=sqrt(-aux1)/(2*A);
         cout<<"Las soluciones imaginarias son: "<<endl;
         cout<<xreal<<"+"<<ximag<<"i "<<endl;
         cout<<xreal<<"-"<<ximag<<"i "<<endl;
-    }
-    else
-    {
-        if (A==0)
-       {
-        cout<<"la ecuacion no es de segundo grado ,la solucion es: "<<C/B;
-       }
-        else
-       {
-              res1=(-B-(sqrt(aux1)))/(2*A);
-            res2=(-B+(sqrt(aux1)))/(2*A);
-            if (res1==res2)
+        }
+        else 
+        {
+            if (aux1==0)
             {
-                cout<<"la solucion para la ecuacion es: "<<res1<<endl;
+                cout<<"la solucion para la ecuacion es: "<<B/(2*A)<<endl;
             }
-            if ((aux1<0)||(A==0))
+            else 
             {
-                cout<<".";
+                res1=(-B-(sqrt(aux1)))/(2*A);
+                res2=(-B+(sqrt(aux1)))/(2*A);
+                if (res1==res2)
+                {
+                  cout<<"la solucion para la ecuacion es: "<<res1<<endl;
+                }
+                else 
+                {
+                    cout<<"las soluciones para la ecuacion son: "<<res1<<" y "<<res2<<endl;
+                }
             }
-            if (res1!=res2) 
-            {
-             cout<<"las soluciones para la ecuacion son: "<<res1<<" y "<<res2<<endl;
-            }
-        
         }
     }
-    
+    system("pause");
 
 }
